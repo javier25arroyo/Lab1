6e60375cac630a7864844e9915ea8c8b84f7af64
Montero04 <monteroedwin620@gmail.com>
2024-06-06 03:13:12 -0600

    Create funcional de Proveedor, Cliente y Empleado v2

--- src/main/java/org/conexionMySql/Main.java
+++ src/main/java/org/conexionMySql/Main.java
@@ -12,13 +12,13 @@ public class Main {
         System.out.printf("Hello MySQL!");
         ConsoleView viewConsole = new ConsoleView();
         ConsoleView consoleView = new ConsoleView();
-//        ClienteController clienteController = new ClienteController(consoleView);
-//        String nombre = "Alberto";
-//        String apellido="Calvo";
-//        String email="celestecalvo@gmail.com";
-//        String telefono="77777777";
-//        Date fecha_registro= Date.valueOf("2024-06-07");
-//        clienteController.agregarClientes(nombre, apellido, email, telefono, fecha_registro);
+        ClienteController clienteController = new ClienteController(consoleView);
+        String nombre = "Alberto";
+        String apellido="Calvo";
+        String email="celestecalvo@gmail.com";
+        String telefono="77777777";
+        Date fecha_registro= Date.valueOf("2024-06-07");
+        clienteController.agregarClientes(nombre, apellido, email, telefono, fecha_registro);
 
         EmpleadoController empleadoController = new EmpleadoController(consoleView);
         String nombreE="Juan";
@@ -28,12 +28,12 @@ public class Main {
         Date fecha_contratacion= Date.valueOf("2024-08-22");
         empleadoController.agregarEmpleado(nombreE,apellidoE,cargo,salario,fecha_contratacion);
 
-//        ProveedorController proveedorController = new ProveedorController(consoleView);
-//        String nombreP="Javier";
-//        String direccion="Cartago";
-//        String telefonoP="88888888";
-//        String emailP="javier@gmail.com";
-//        Date fecha_registroP=Date.valueOf("2025-12-25");
-//        proveedorController.agregarProveedor(nombreP,direccion,telefonoP,emailP,fecha_registroP);
+        ProveedorController proveedorController = new ProveedorController(consoleView);
+        String nombreP="Javier";
+        String direccion="Cartago";
+        String telefonoP="88888888";
+        String emailP="javier@gmail.com";
+        Date fecha_registroP=Date.valueOf("2025-12-25");
+        proveedorController.agregarProveedor(nombreP,direccion,telefonoP,emailP,fecha_registroP);
     }
 }
