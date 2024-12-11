19e82f84dceb63c0c702bef72a21a5ee4b7b1e10
Javier Arroyo Rojas <86391507+javier25arroyo@users.noreply.github.com>
2024-12-10 21:44:42 -0600

    Update0.1

--- src/main/java/model/Conexion.java
+++ src/main/java/model/Conexion.java
@@ -5,17 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://45.88.196.5:3306/u484426513_diseno224";
-    private static final String USER = "u484426513_diseno224";
-    private static final String PASSWORD = "#7cYr646u@*Rp.P";
-
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection(){
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch ( SQLException e){
-            System.err.println("Fallo la conexion" + e.getMessage());
+            System.err.println("Fallo la conexion: " + e.getMessage());
         }
         return connection;
     }
